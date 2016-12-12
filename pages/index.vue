7155e01975c166e46547add4e38ac6b65bf72631
lynzz <lynzz168@gmail.com>
2016-12-12 17:35:48 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <img src="~static/logo.png" alt="Nuxt.js Logo" />
+    <h1 class="title">
+      A minimalistic framework <br>for server-rendered Vue.js applications
+    </h1>
+    <router-link class="button" to="/about">
+      About page
+    </router-link>
+  </section>
+</template>
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
